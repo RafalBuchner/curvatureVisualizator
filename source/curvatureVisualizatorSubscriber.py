5b44678d13d76507694de7d6eb3bd07f1fdd7e1a
rafal <rafal.buchner@gmail.com>
2023-07-25 01:24:02 +0200

    subscriber update

--- source/curvatureVisualizatorSubscriber.py
+++ source/curvatureVisualizatorSubscriber.py
@@ -1,21 +1,71 @@
+from fontTools.pens.transformPen import TransformPointPen, TransformPen
+from fontTools.misc.transform import Transform
 from displaySubscriber import DisplaySuscriber
 from mojo import subscriber
+from merz import MerzPen
+from settings import (
+    internalGetDefault,
+    internalSetDefault,
+    extensionID,
+    extensionKeyStub
+)
 
 # ====================================
-# Example
+# S U B S C R I B E R
 # ====================================       
 
-class CurvatureVisualizatorSubscriber(subscriber.DisplaySuscriber):
+class CurvatureVisualizatorSubscriber(DisplaySuscriber):
     debug = True
 
-    title = "this is my subscriber button" 
-
-    def menuButtonWasPressed(self, nsMenuItem):
-        print(f"current state of '{self.title}' button: {self.getButtonState()}")        
-        print(nsMenuItem)
+    title = "curvature visualizator" 
+    curvaturePath = None
     
+    def build(self):
+        self.showMe = internalGetDefault("isVisible")
+        window = self.getGlyphEditor()
+        self.backgroundContainer = window.extensionContainer(
+            identifier=extensionKeyStub + "background",
+            location="background",
+            clear=True,
+        )
+        self.bgBaseLayer = self.backgroundContainer.appendBaseSublayer()
+        self.curvaturePath = self.bgBaseLayer.appendPathSublayer(
+            fillColor=(1, 0, 0, 0.5), visible=False
+        )
+
+    def toggleOn(self):
+        if self.curvaturePath is None:
+            return
+        self.curvaturePath.setVisible(True)
+
+    def toggleOff(self):
+        if self.curvaturePath is None:
+            return
+        self.curvaturePath.setVisible(False)
+
+    def glyphEditorDidKeyDown(self, info):
+        self.drawPath(info)
+
+    def glyphEditorDidUndo(self, info):
+        self.drawPath(info)
+
+    def glyphEditorDidMouseDrag(self, info):
+        self.drawPath(info)
+
     def glyphEditorDidOpen(self, info):
-        super().glyphEditorDidOpen(info) 
-        print("glyph editor did open!!!")
+        super().glyphEditorDidOpen(info)
+        self.drawPath(info)
+
+    def drawPath(self, info):
+        glyph = info["glyph"]
+        merzPen = MerzPen()
+        n_glyph = RGlyph()
+        n_glyphPen = n_glyph.getPen()
+        glyph.draw(n_glyphPen)
+        n_glyph.moveBy((100,100))
+        n_glyph.draw(merzPen)
+
+        self.curvaturePath.setPath( merzPen.path )
+        
 
 subscriber.registerGlyphEditorSubscriber(CurvatureVisualizatorSubscriber)
--- source/displaySubscriber.py
+++ source/displaySubscriber.py
@@ -1,6 +1,11 @@
 from vanilla.vanillaBase import VanillaCallbackWrapper
 from mojo.subscriber import Subscriber
 from AppKit import NSMenuItem
+from settings import (
+    internalGetDefault,
+    internalSetDefault,
+    extensionID
+)
 
 def _getDisplayPopUpMenuFromEditWindow(editWindow):
     if editWindow is None:
@@ -19,14 +24,35 @@ def _createCustomVisualizerSeparator(displayPopUp):
         menu = displayPopUp.menu()
         menu.addItem_(NSMenuItem.separatorItem())
 
-# ====================================
-# S U B S C R I B E R
-# ====================================
+# =========================================
+#  S U B S C R I B E R (with basic events)
+# =========================================
 
 class DisplaySuscriber(Subscriber):
     debug = True
 
     title = None
+
+    def menuButtonWasPressed(self, nsMenuItem):
+        if self.getButtonState():
+            self.showMe = True
+        else:
+            self.showMe = False
+
+    @property
+    def showMe(self):
+        return self._showMe
+
+    @showMe.setter
+    def showMe(self, value):
+        if value:
+            self.toggleOn()
+        
+        else:
+            self.toggleOff()
+
+        internalSetDefault("isVisible", value)
+        self._showMe = value
 
     def getButtonState(self):
         return bool(self._menuItem.state())
@@ -34,9 +60,6 @@ class DisplaySuscriber(Subscriber):
     def _menuItemCallback(self, nsMenuItem):
         self._menuItem.setState_(not self._menuItem.state())
         self.menuButtonWasPressed(nsMenuItem)
-
-    def menuButtonWasPressed(self, nsMenuItem):
-        NotImplemented
 
     def _appendMenuItemToDisplayMenu(self, window):
         assert self.title is not None, "title wasn't set"
@@ -57,11 +80,19 @@ class DisplaySuscriber(Subscriber):
         menu = displayPopUp.menu()
         menu.addItem_(self._menuItem)
 
+    def toggleOff(self):
+        NotImplemented
+
+    def toggleOn(self):
+        NotImplemented
+
     def glyphEditorDidOpen(self, info):
         """
         Whenever you want to use glyphEditorDidOpen event in your own DisplaySuscriber class, please
         use super method of glyphEditorDidOpen
         """
         self._appendMenuItemToDisplayMenu(info["glyphEditor"])
+        self.showMe = internalGetDefault("isVisible")
+        self._menuItem.setState_(self.showMe)
  
 
